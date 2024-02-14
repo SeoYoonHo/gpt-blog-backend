5385456b5b612ccdc65b80de07b725c5d6c39ff4
seoyoonho <gogoy2643@naver.com>
2024-02-14 23:34:48 +0900

    first

---
+++ src/main/java/com/syh/gptblog/GptblogApplication.java
@@ -0,0 +1,13 @@
+package com.syh.gptblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syh/gptblog/GptblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syh.gptblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
