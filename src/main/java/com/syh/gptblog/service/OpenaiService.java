b434159f1d00c51c6e76199c94e1fc73b8a89fc0
seoyoonho <gogoy2643@naver.com>
2024-02-23 17:38:07 +0900

    쿼리 수정해야함

--- src/main/java/com/syh/gptblog/service/OpenaiService.java
+++ src/main/java/com/syh/gptblog/service/OpenaiService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.syh.gptblog.api.ApiUtil;
 import com.syh.gptblog.domain.Post;
 import com.syh.gptblog.dto.PostDto;
+import com.syh.gptblog.mapper.PostMapper;
 import com.syh.gptblog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,17 +25,25 @@ public class OpenaiService {
 //        OpenApiDto.ApiResponseDto apiResponseDto = apiUtil.getGptAnswer(getGptAnswerRequest);
 //
 //        // 1.1 응답 json -> Post 객체 변환
-//        Post post = new Post();
+        Post post = new Post();
+        post.setSlug("my-first-post");
+        post.setTitle("My First Post");
+        post.setContent("This is the content of my first post.");
+        post.setCover("coverImageUrl");
+        post.setDate_a("2021-01-01");
+        post.setPublished("true");
+        post.setLastEditedAt("2021-01-02");
+        post.setBlurUrl("blurImageUrl");
+
 //        post.setContent(apiResponseDto.getChoices().get(0).getText());
 //
 //        // 2.해당 응답 게시글 db에 저장하기
-//        postRepository.save(post);
-//
-//        // 3.응답 게시글 내려주기
-//        // 3.1. Post -> PostResponseDto
-//        PostDto.GptAnswerResponse gptAnswerResponse = PostMapper.INSTANCE.postToGptResponseDto(post);
-        PostDto.GptAnswerResponse gptAnswerResponse = new PostDto.GptAnswerResponse();
-        gptAnswerResponse.setContent("testContent!!!!!!!!!!!!!!!");
+        postRepository.save(post);
+
+        // 3.응답 게시글 내려주기
+        // 3.1. Post -> PostResponseDto
+        PostDto.GptAnswerResponse gptAnswerResponse = PostMapper.INSTANCE.postToGptResponseDto(post);
+//        PostDto.GptAnswerResponse gptAnswerResponse = new PostDto.GptAnswerResponse();
 
         return gptAnswerResponse;
     }
